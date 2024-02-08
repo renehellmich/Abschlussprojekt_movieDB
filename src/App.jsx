338681dd50d289bad7195b199f4251ca8d75ee96
“StefanAiroaie” <airoaiestefan@gmail.com>
2024-02-08 15:21:37 +0100

    fetch mit keyword für search

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import SearchBar from './components/serachBar/SearchBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+
+      <SearchBar />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,6 +31,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
     </>
   )
 }
---
+++ src/components/serachBar/SearchBar.jsx
@@ -0,0 +1,53 @@
+import "./searchBar.css"
+import { useEffect, useRef, useState } from "react"
+import axios from "axios"
+const SearchBar = () => {
+
+    const searchInputRef = useRef()
+
+    // let searchInputRef = "the"
+    let apiKey = "1f06982c9b50c78835d9370e1b4a9b83"
+    let apiLinkSerachKeyword = `https://api.themoviedb.org/3/search/keyword?query=${searchInputRef}&page=1&api_key=`
+
+    console.log(searchInputRef);
+
+
+
+    useEffect(() => {
+        const apiFetch = async () => {
+            const raspuns = await axios.get(`${apiLinkSerachKeyword}${apiKey}`)
+            console.log("s", raspuns.data);
+
+        }
+        apiFetch()
+    }, [])
+
+    const movieSearch = () => {
+        console.log("searchInputRef", searchInputRef.current.value);
+
+    }
+
+
+
+    return (
+        <>
+
+
+
+
+            <h1>hier is search bar</h1>
+
+            <div className=''>
+                <input
+                    style={{ width: "100%" }}
+                    type="text"
+                    ref={searchInputRef}
+                    onChange={movieSearch}
+                    placeholder={`search for a movie`}
+                />
+            </div>
+        </>
+    );
+}
+
+export default SearchBar;
